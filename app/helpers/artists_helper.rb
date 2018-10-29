2625d6a4a0dd4f670f2e7a44004949dcb43cad43
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-29 18:02:54 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -3,6 +3,6 @@ module ArtistsHelper
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
   
-  def artist_select()
+  def artist_select(song)
   end
 end
