48029001bacd594f970d3432b9c09bc7ebeb73e3
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-29 19:29:01 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,5 @@
 module SongsHelper
-def artist_id_field(song)
+def author_id_field(post)
     if post.author.nil?
       select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
     else
